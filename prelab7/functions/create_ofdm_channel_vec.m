093fa410382909da5309bccb6bba36b81f0d451c
Steven <level2fast@gmail.com>
2024-03-03 18:05:09 -0800

    completed matlab sim
    completed matlab sim

---
+++ prelab7/functions/create_ofdm_channel_vec.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = create_ofdm_channel_vec(inputArg1,inputArg2)
+%CREATE_OFDM_CHANNEL_VEC Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
