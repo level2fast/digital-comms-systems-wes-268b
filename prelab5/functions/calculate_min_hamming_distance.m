a6d47fb0921a3b6f813f43a28953dbacd4456c7b
level2fast <level2fast@gmail.com>
2025-04-03 15:57:49 -0700

    Update calculate_min_hamming_distance.m

--- prelab5/functions/calculate_min_hamming_distance.m
+++ prelab5/functions/calculate_min_hamming_distance.m
@@ -2,9 +2,6 @@ function min_distance = calculate_min_hamming_distance(codeword1, codeword2)
 % Example:
 %   codeword1 = [0 1 1 0];
 %   codeword2 = [1 1 1 0];
-% 
-%   min_distance = calculate_min_hamming_distance(codeword1, codeword2);
-%   disp(['Minimum Hamming distance between codewords: ' num2str(min_distance)]);
 
     % Ensure codewords have the same length
     if length(codeword1) ~= length(codeword2)
